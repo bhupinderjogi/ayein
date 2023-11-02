6f9b81ced6b9fde933cc364e1ffa8704ef0081d7
bhupinderjogi <149693110+bhupinderjogi@users.noreply.github.com>
2023-11-02 23:37:33 +0530

    Update code.c

--- code.c
+++ code.c
@@ -89,35 +89,6 @@ closegraph();
 return 0;
 }
 
-//SCALING//
-#include<stdio.h>
-#include<graphics.h>
-#include<stdlib.h>
-int main(){
-int gm,gd=DETECT,x1,x2,x3,y1,y2,y3,nx1,nx2,nx3,ny1,ny2,ny3,xt,yt,c,r,sx,sy;
-float t;
-printf("\t program for basic transaction");
-printf("\n\t Enter the Points of Triangle");
-scanf("%d%d%d%d%d%d",&x1,&x2,&x3,&y1,&y2,&y3);
-printf("\n Enter the scaling Factor");
-scanf("%d%d",&xt,&yt);
-initgraph(&gd,&gm,"");
-line(x1,y1,x2,y2);
-line(x2,y2,x3,y3);
-line(x3,y3,x1,y1);
-nx1=x1*xt;
-nx2=x2*xt;
-nx3=x3*xt;
-ny1=y1*xt;
-ny2=y2*xt;
-ny3=y3*xt;
-line(nx1,ny1,nx2,ny2);
-line(nx2,ny2,nx3,ny3);
-line(nx3,ny3,nx1,ny1);
-delay(50000);
-closegraph();
-return 0;
-}
 
 //FLOOD FILL//
 #include<stdio.h>
